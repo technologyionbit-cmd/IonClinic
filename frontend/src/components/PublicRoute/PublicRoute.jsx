5268cb55da45971e21e071611052be7f193ccd3e
barakat <ahmed0barakat001@gmail.com>
2026-03-29 13:36:32 +0200

    edit

--- frontend/src/components/PublicRoute/PublicRoute.jsx
+++
@@ -1,6 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-export default function PublicRoute() {
-  const token = localStorage.getItem("token");
-  return token ? <Navigate to="/" replace /> : <Outlet />;
-}
